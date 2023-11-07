19cb955274da76abcfb1556c72d58f26e9845f79
AnonymousFromGeorgia <52291420+AnonymousFromGeorgia@users.noreply.github.com>
2023-11-07 21:19:02 +0400

    Update Bloodhound.py

--- Bloodhound.py
+++ Bloodhound.py
@@ -1,58 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import os
+import signal
+import requests
+
 os.system('clear')
 
-import signal
-
 def keyboardInterruptHandler(signal, frame):
-    print("\nპროგრამა გაითიშა.".format(signal))
+    print("\nპროგრამა გაითიშა.")
     exit(0)
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
-from urllib2 import Request, urlopen, URLError, HTTPError
-
-def Space(j):
-	i = 0
-	while i<=j:
-		print " ",
-		i+=1
-
-
 def findAdmin():
-	f = open("სია.txt","r");
-	link = raw_input("შეიყვანეთ სასურველი ვებსაიტის ბმული \n(მაგ: nimushi.ge ან www.nimushi.com): ")
-	
-	print "\nმიმდინარეობს " + link + "-ის სკანირება...\n"
-	
-	while True:
-		sub_link = f.readline()
-		if not sub_link:
-			break
-		req_link = "http://"+link+"/"+sub_link
-		req = Request(req_link)
-		try:
-			response = urlopen(req)
-		except HTTPError as e:
-			continue
-		except URLError as e:
-			continue
-		else:
-			print "OK => ",req_link
+    with open("სია.txt", "r") as f:
+        link = input("შეიყვანეთ სასურველი ვებსაიტის ბმული \n(მაგ: nimushi.ge ან www.nimushi.com): ")
+
+        print("\nმიმდინარეობს " + link + "-ის სკანირება...\n")
+
+        for sub_link in f:
+            sub_link = sub_link.strip()
+            req_link = f"https://{link}/{sub_link}"
+            try:
+                response = requests.get(req_link)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                continue
+            except requests.exceptions.RequestException as e:
+                continue
+            else:
+                print("OK => ", req_link)
 
 def Credit():
-	print("           __     ")
-	print("      (___()'`;   ")
-	print("      /,    /`    ")
-	print("     // ''--\     ")
-	print("   Bloodhound.py\n")
-	print("▶ Coded by გიო რგი ◀\n")
-	print("https://github.com/AnonymousFromGeorgia/Bloodhound\n")
-	print("ℹ️უფასო ხელსაწყო საიტებზე Admin Panel-ების მოსაძებნად (Admin Panel Finder)\n")
-	print("ℹ️პროგრამის გასათიშად გამოიყენეთ კლავიშების კომბინაცია CTRL+C\n")
-	print("ℹ️შეიყვანეთ რეალური ბმული, წინააღმდეგ შემთხვევაში ხელსაწყო არ იმუშავებს\n")
+    print("           __     ")
+    print("      (___()'`;   ")
+    print("      /,    /`    ")
+    print("     // ''--\     ")
+    print("   Bloodhound.py\n")
+    print("▶ Coded by გიო რგი ◀\n")
+    print("https://github.com/AnonymousFromGeorgia/Bloodhound\n")
+    print("> უფასო ხელსაწყო საიტებზე Admin Panel-ების მოსაძებნად (Admin Panel Finder)\n")
+    print("> პროგრამის გასათიშად გამოიყენეთ კლავიშების კომბინაცია CTRL+C\n")
+    print("> შეიყვანეთ რეალური ბმული, წინააღმდეგ შემთხვევაში ხელსაწყო არ იმუშავებს\n")
 
 Credit()
 findAdmin()
